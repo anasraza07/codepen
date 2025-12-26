f9ace4719aa503e0ae9e54e88d97763486a2ab25
Anasraza <ar470684@gmail.com>
2025-12-27 00:07:23 +0500

    updated border color + applied collapsible

--- app/page.tsx
+++ app/page.tsx
@@ -48,9 +48,9 @@ export default function Home() {
     <div className="min-h-screen">
       <Group orientation="vertical" className="h-screen">
         <Panel defaultSize={"60%"} minSize={"20%"}>
-          <Group className="bg-[#1C1C1C] h-full">
+          <Group className="bg-[#1C1C1C] h-full flex gap-4 pb-4">
 
-            <Panel minSize={"5%"} className="border border-[#636363]">
+            <Panel minSize={"5%"} className="border border-[#494949]">
               <span className="bg-[#1D1E22] text-[#AAAEBC] font-semibold inline-block py-2 px-4 w-fit">
                 HTML
               </span>
@@ -60,7 +60,7 @@ export default function Home() {
             </Panel>
 
             <Panel minSize={"5%"} className="border
-            border-[#636363]">
+            border-[#494949]">
               <span className="bg-[#1D1E22] text-[#AAAEBC] font-semibold inline-block py-2 px-4 w-fit">
                 CSS
               </span>
@@ -68,7 +68,7 @@ export default function Home() {
                 onChange={(value) => debouncedPreview(
                   value, "css")} />
             </Panel>
-            <Panel minSize={"5%"} className="border border-[#636363]">
+            <Panel minSize={"5%"} className="border border-[#494949]">
               <span className="bg-[#1D1E22] text-[#AAAEBC] font-semibold inline-block py-2 px-4 w-fit">
                 JS
               </span>
@@ -78,7 +78,7 @@ export default function Home() {
           </Group>
         </Panel>
 
-        <Panel>
+        <Panel collapsible>
           <iframe className="w-full h-full bg-white" srcDoc={`
             <head>
               <style>${cssCode}</style>
@@ -89,21 +89,23 @@ export default function Home() {
               `}></iframe>
         </Panel>
 
-        <Panel defaultSize={"90px"}> 
+        <Panel defaultSize={"90px"}>
           <div className="fixed bottom-0 left-0 right-0 max-h-40 overflow-hidden flex flex-col">
-            <div className="bg-gray-800 py-1 px-2 order-3">
-              <button className="bg-gray-500 text-[12px] rounded-sm py-1 px-2 cursor-pointer">Console</button>
-            </div>
-            <div className="console-input bg-gray-700 py-1 px-2 flex items-center space-x-2 order-2">
-              <span className="font-semibold">&gt;</span>
-              <textarea className="flex-1 border-none outline-none overflow-y-hidden resize-none field-sizing-content"></textarea>
-            </div>
-            <div className="console-tab bg-[#1C1C1C] py-1 px-2 flex items-center justify-between space-x-2 order-1">
+            <div className="console-tab bg-[#1C1C1C] py-1 px-2 flex items-center justify-between space-x-2">
               <span className="font-semibold inline-block">Console</span>
               <div className="flex gap-2 text-right">
                 <button className="bg-gray-600 py-1 px-2 text-sm">Clear</button>
                 <button className="bg-gray-600 py-1 px-2 text-sm">X</button>
               </div>
+            </div>
+
+            <div className="console-input bg-gray-700 py-1 px-2 flex items-center space-x-2">
+              <span className="font-semibold">&gt;</span>
+              <textarea className="flex-1  outline-none overflow-y-hidden resize-none field-sizing-content"></textarea>
+            </div>
+
+            <div className="bg-gray-800 py-1 px-2">
+              <button className="bg-gray-500 text-[12px] rounded-sm py-1 px-2 cursor-pointer">Console</button>
             </div>
           </div>
         </Panel>
